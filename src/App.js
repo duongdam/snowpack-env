16f0f43ba2b2de7bb9b657cf050f886b59b45931
duongdam <duong.dam@classfunc.com>
2020-08-05 16:33:21 +0700

    change name

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
         <div className="App">
           <Switch>
             <Route exact path={'/'} component={Home}/>
+
             {/*<Route path={'/event'} component={Events}/>*/}
           </Switch>
         </div>
